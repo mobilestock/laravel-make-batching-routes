e65ba727df5b3af9368454b2d1fe829708b74137
SavioSReis <hjd44.sa@gmail.com>
2025-04-17 10:31:16 -0300

    refactor: remove uso da classe ReflectionClass e simplifica a lógica de middlewares

--- src/HasBatchingFindEndpoint.php
+++ src/HasBatchingFindEndpoint.php
@@ -4,7 +4,6 @@ namespace MobileStock\MakeBatchingRoutes;
 
 use Illuminate\Auth\Middleware\Authenticate;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use ReflectionClass;
 
 /**
  * @property array<class-string> $middlewares
@@ -13,16 +12,9 @@ trait HasBatchingFindEndpoint
 {
     use HasFactory;
 
-    /**
-     * @var array<class-string>
-     */
-    protected static $defaultMiddlewares = [Authenticate::class];
-
     protected static function getBatchingMiddlewares(): array
     {
-        $middlewares = (new ReflectionClass(static::class))->hasProperty('middlewares')
-            ? static::$middlewares
-            : static::$defaultMiddlewares;
+        $middlewares = static::$middlewares ?? [Authenticate::class];
 
         return $middlewares;
     }
