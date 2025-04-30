33209bbed5e1813c7ddc4140ac0fd314a07c204e
SavioSReis <hjd44.sa@gmail.com>
2025-04-30 09:29:59 -0300

    test: adiciona testes para verificar o comportamento da busca de tabelas com e sem ordenação

--- tests/Feature/BatchingControllerTest.php
+++ tests/Feature/BatchingControllerTest.php
@@ -1,5 +1,8 @@
 <?php
 
+use Illuminate\Database\Connection;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Database\Eloquent\Model;
 use MobileStock\MakeBatchingRoutes\Http\Controllers\Batching;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
@@ -52,6 +55,45 @@ it('should jump if not find the table', function () use ($MODEL_PATH) {
     $controller = new Batching();
     $controller->find();
 })->throws(NotFoundHttpException::class, 'Model não encontrada pra tabela: /');
+
+dataset('datasetControllerFindWorks', function () {
+    return [
+        'without sorting' => [[], [['id' => 3], ['id' => 1], ['id' => 2]]],
+        'with sorting' => [['id' => [3, 2, 1]], [['id' => 3], ['id' => 2], ['id' => 1]]],
+    ];
+});
+
+it('should work correctly :dataset', function (array $parameters, array $expected) use ($MODEL_PATH) {
+    $request = Request::create('api/batching/find_tables', parameters: $parameters);
+    Request::swap($request);
+    App::partialMock()
+        ->shouldReceive('getNamespace')
+        ->twice()
+        ->andReturn('Tests\\Temp\\')
+        ->shouldReceive('path')
+        ->with('Models')
+        ->twice()
+        ->andReturn('/laravel-make-batching-routes/tests/Temp/Models');
+    File::put(
+        "$MODEL_PATH/FindTable.php",
+        '<?php namespace Tests\Temp\Models; class FindTable extends \Illuminate\Database\Eloquent\Model {}'
+    );
+
+    $pdoMock = Mockery::mock(PDO::class);
+
+    $connectionMock = Mockery::mock(Connection::class)->makePartial();
+    $connectionMock->__construct($pdoMock);
+    $connectionMock->shouldReceive('select')->andReturn([['id' => 3], ['id' => 1], ['id' => 2]]);
+
+    $resolverMock = Mockery::mock(DatabaseManager::class);
+    $resolverMock->shouldReceive('connection')->andReturn($connectionMock);
+
+    Model::setConnectionResolver($resolverMock);
+
+    $controller = new Batching();
+    $response = $controller->find();
+    expect($response)->toBe($expected);
+})->with('datasetControllerFindWorks');
 
 afterAll(function () use ($MODEL_PATH) {
     File::deleteDirectory($MODEL_PATH);
