aac36f06283b2ef2f59ad2fad258608f144859b5
SavioSReis <hjd44.sa@gmail.com>
2025-04-30 09:40:09 -0300

    test: refatora os testes para verificar comportamento ao não encontrar arquivo, classe ou tabela

--- tests/Feature/BatchingControllerTest.php
+++ tests/Feature/BatchingControllerTest.php
@@ -13,48 +13,31 @@ beforeEach(function () use ($MODEL_PATH) {
     File::ensureDirectoryExists($MODEL_PATH);
 });
 
-it('should jump if file does not exist', function () use ($MODEL_PATH) {
-    App::partialMock()
-        ->shouldReceive('getNamespace')
-        ->once()
-        ->andReturn('Tests\\Temp\\')
-        ->shouldReceive('path')
-        ->with('Models')
-        ->once()
-        ->andReturn('/laravel-make-batching-routes/tests/Temp/Models');
+dataset('datasetControllerFindFails', function () use ($MODEL_PATH) {
+    return [
+        'file' => ["$MODEL_PATH/NotFindFile.txt", ''],
+        'class' => ["$MODEL_PATH/NotFindClass.php", '<?php namespace Fake\Models; class NotFindClass {}'],
+        'table' => [
+            "$MODEL_PATH/NotFindTable.php",
+            '<?php namespace Tests\Temp\Models; class NotFindTable extends \Illuminate\Database\Eloquent\Model {}',
+        ],
+    ];
+});
 
-    $controller = new Batching();
-    $controller->find();
-})->throws(NotFoundHttpException::class, 'Model não encontrada pra tabela: /');
-
-it('should jump if class does not exist', function () use ($MODEL_PATH) {
-    App::partialMock()
-        ->shouldReceive('getNamespace')
-        ->andReturn('Fake\\')
-        ->shouldReceive('path')
-        ->with('Models')
-        ->andReturn($MODEL_PATH);
-    File::put("$MODEL_PATH/NotFindClass.php", '<?php namespace Tests\Temp\Models; class NotFindClass {}');
-
-    $controller = new Batching();
-    $controller->find();
-})->throws(NotFoundHttpException::class, 'Model não encontrada pra tabela: /');
-
-it('should jump if not find the table', function () use ($MODEL_PATH) {
+it('should throws exception if no :dataset is found', function (string $filePath, string $fileContent) {
     App::partialMock()
         ->shouldReceive('getNamespace')
         ->andReturn('Tests\\Temp\\')
         ->shouldReceive('path')
         ->with('Models')
         ->andReturn('/laravel-make-batching-routes/tests/Temp/Models');
-    File::put(
-        "$MODEL_PATH/NotFindTable.php",
-        '<?php namespace Tests\Temp\Models; class NotFindTable extends \Illuminate\Database\Eloquent\Model {}'
-    );
+    File::put($filePath, $fileContent);
 
     $controller = new Batching();
     $controller->find();
-})->throws(NotFoundHttpException::class, 'Model não encontrada pra tabela: /');
+})
+    ->with('datasetControllerFindFails')
+    ->throws(NotFoundHttpException::class, 'Model não encontrada pra tabela: /');
 
 dataset('datasetControllerFindWorks', function () {
     return [
