403601634b10d592f93d3286f53090618861ee8b
SavioSReis <hjd44.sa@gmail.com>
2025-04-30 10:20:49 -0300

    test: adiciona uso da classe Str e refatora limpeza de diretórios temporários

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 uses(Tests\TestCase::class)->in(__DIR__);
 
 function invokeProtectedMethod(object $class, string $methodName, array $parameters = []): mixed
@@ -8,3 +10,16 @@ function invokeProtectedMethod(object $class, string $methodName, array $paramet
     $method->setAccessible(true);
     return $method->invokeArgs($class, $parameters);
 }
+
+pest()->afterEach(function () {
+    $tempDirectory = __DIR__ . '/Temp';
+    $tempFile = new RecursiveDirectoryIterator($tempDirectory);
+
+    foreach ($tempFile as $file) {
+        if ($file->isFile() || Str::endsWith($file->getFilename(), '.')) {
+            continue;
+        }
+
+        File::deleteDirectory($file->getPathname());
+    }
+});
