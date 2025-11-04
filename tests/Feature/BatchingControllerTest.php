d972f4f21334e0034733ca0b9bef4eda77f41e63
SavioSReis <hjd44.sa@gmail.com>
2025-11-04 10:18:50 -0300

    fix: remover parâmetro 'without_scopes' do dataset de sucesso

--- tests/Feature/BatchingControllerTest.php
+++ tests/Feature/BatchingControllerTest.php
@@ -57,7 +57,7 @@ it('should throws exception if no :dataset is found', function (string $filePath
 dataset('datasetControllerFindSucceeds', function () {
     return [
         'with' => [
-            'parameters' => ['id' => [3, 2, 1], 'order_by_field' => 'id', 'without_scopes' => true],
+            'parameters' => ['id' => [3, 2, 1], 'order_by_field' => 'id'],
             'expected' => [['id' => 3], ['id' => 2], ['id' => 1]],
         ],
         'without' => [
